0fbb972e4e94190f65549e9ab791244d72f82453
sabir0722 <140849575+sabir0722@users.noreply.github.com>
2023-12-20 21:21:38 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+<opencv_storage>
+<cascade type_id="opencv-cascade-classifier"><stageType>BOOST</stageType>
+  <featureType>HAAR</featureType>
+  <height>24</height>
+  <width>24</width>
+  <stageParams>
+    <maxWeakCount>211</maxWeakCount></stageParams>
+  <featureParams>
+    <maxCatCount>0</maxCatCount></featureParams>
+  <stageNum>25</stageNum>
+  <stages>
+    <_>
+      <maxWeakCount>9</maxWeakCount>
+      <stageThreshold>-5.0425500869750977e+00</stageThreshold>
+      <weakClassifiers>
+        <_>
+          <internalNodes>
+            0 -1 0 -3.1511999666690826e-02</internalNodes>
+          <leafValues>
+            2.0875380039215088e+00 -2.2172100543975830e+00</leafValues></_>
+        <_>
+          <internalNodes>
+            0 -1 1 1.2396000325679779e-02</internalNodes>
+          <leafValues>
+            -1.8633940219879150e+00 1.3272049427032471e+00</leafValues></_>
+        <_>
+          <internalNodes>
+            0 -1 2 2.1927999332547188e-02</internalNodes>
+          <leafValues>
+            -1.5105249881744385e+00 1.0625729560852051e+00</leafValues></_>
+        <_>
+          <internalNodes>
+            0 -1 3 5.7529998011887074e-03</internalNodes>
+          <leafValues>
+            -8.7463897466659546e-01 1.1760339736938477e+00</leafValues></_>
+        <_>
+          <internalNodes>
+            0 -1 4 1.5014000236988068e-02</internalNodes>
+          <leafValues>
+            -7.7945697307586670e-01 1.2608419656753540e+00</leafValues></_>
+        <_>
+          <internalNodes>
+            0 -1 5 9.9371001124382019e-02</internalNodes>
+          <leafValues>
+            5.5751299858093262e-01 -1.8743000030517578e+00</leafValues></_>
+        <_>
+          <internalNodes>
+            0 -1 6 2.7340000960975885e-03</internalNodes>
+          <leafValues>
+            -1.6911929845809937e+00 4.4009700417518616e-01</leafValues></_>
+        <_>
+          <internalNod
